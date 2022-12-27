910584962ee89160bae3bc51efb5c089c1f4d45f
Irene_Godoy_del_Pozo <irenegdp@live.com>
2022-12-27 10:36:53 +0100

    Project Created

---
+++ Source/Arkanoid_Irene/Arkanoid_Irene.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Arkanoid_Irene.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Arkanoid_Irene, "Arkanoid_Irene" );
---
+++ Source/Arkanoid_Irene/Arkanoid_IreneGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Arkanoid_IreneGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARKANOID_IRENE_API AArkanoid_IreneGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
