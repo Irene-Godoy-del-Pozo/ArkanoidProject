b3fc3fe4cb08960c1795f292b71368738473a63a
Irene_Godoy_del_Pozo <irenegdp@live.com>
2022-12-28 11:43:51 +0100

    Brick Created

---
+++ Source/Arkanoid_Irene/Brick.cpp
@@ -0,0 +1,68 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Brick.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/BoxComponent.h"
+#include "DrawDebugHelpers.h"
+
+#include "Bullet.h"
+
+
+// Sets default values
+ABrick::ABrick()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	BrickMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BrickMesh"));
+
+	BrickMesh->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+
+	Box_Collision = CreateDefaultSubobject<UBoxComponent>(TEXT("Box Collision"));
+	Box_Collision->SetBoxExtent(FVector(25.0f, 10.0f, 10.0f));
+
+	RootComponent = Box_Collision;
+}
+
+// Called when the game starts or when spawned
+void ABrick::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	Box_Collision->OnComponentBeginOverlap.AddDynamic(this, &ABrick::OnOverlapBegin);
+}
+
+void ABrick::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndexType, bool bFromSweep, const FHitResult& SweepResult)
+{
+	GEngine->AddOnScreenDebugMessage(-1, 10.f, FColor::Blue, FString::Printf(TEXT("Overlap event")));
+	if (OtherActor->ActorHasTag("Bullet")) {
+
+		GEngine->AddOnScreenDebugMessage(-1, 10.f, FColor::Green, FString::Printf(TEXT("Is Bullet")));
+
+		ABullet* MyBullet = Cast<ABullet>(OtherActor);
+
+		FVector BulletVelocity = MyBullet->GetVelocity();
+		BulletVelocity *= bounceSpeed;
+
+		MyBullet->GetBullet()->SetPhysicsLinearVelocity(BulletVelocity, true);
+
+		FTimerHandle UnusedHandle;
+		GetWorldTimerManager().SetTimer(UnusedHandle, this, &ABrick::DestroyBrick, 0.1f, false);
+
+
+	}
+}
+
+void ABrick::DestroyBrick()
+{
+	this->Destroy();
+}
+
+// Called every frame
+void ABrick::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
