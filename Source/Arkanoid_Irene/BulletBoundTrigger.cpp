80a8e906712a6f861091d467495d696fa255f6cd
Irene_Godoy_del_Pozo <irenegdp@live.com>
2022-12-28 19:26:23 +0100

    Bottom bullet boundary

---
+++ Source/Arkanoid_Irene/BulletBoundTrigger.cpp
@@ -0,0 +1,50 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BulletBoundTrigger.h"
+#include "Components/BoxComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "DrawDebugHelpers.h"
+
+#include "Vaus_Controller.h"
+
+// Sets default values
+ABulletBoundTrigger::ABulletBoundTrigger()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	Box_Collision = CreateDefaultSubobject<UBoxComponent>(TEXT("Box Collision"));
+	RootComponent = Box_Collision;
+
+	Box_Collision->SetBoxExtent(FVector(380.0f, 10.0f, 50.0f));
+}
+
+// Called when the game starts or when spawned
+void ABulletBoundTrigger::BeginPlay()
+{
+	Super::BeginPlay();
+
+	Box_Collision->OnComponentBeginOverlap.AddDynamic(this, &ABulletBoundTrigger::OnOverlapBegin);
+
+	_vausController = Cast<AVaus_Controller>(UGameplayStatics::GetPlayerController(GetWorld(), 0));
+}
+
+void ABulletBoundTrigger::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndexType, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (OtherActor->ActorHasTag("Bullet")) {
+
+		OtherActor->Destroy();
+
+		_vausController->SpawnNewBullet();
+
+	}
+}
+
+// Called every frame
+void ABulletBoundTrigger::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/Arkanoid_Irene/BulletBoundTrigger.h
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "BulletBoundTrigger.generated.h"
+
+class UBoxComponent;
+class AVaus_Controller;
+
+UCLASS()
+class ARKANOID_IRENE_API ABulletBoundTrigger : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ABulletBoundTrigger();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+		UBoxComponent* Box_Collision;
+
+	UFUNCTION()
+		void OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor,
+							class UPrimitiveComponent* OtherComp, int32 OtherBodyIndexType, 
+							bool bFromSweep, const FHitResult& SweepResult);
+
+
+
+	AVaus_Controller* _vausController;
+
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
